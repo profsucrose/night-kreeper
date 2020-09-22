d44b831c8d8057fa435db516e6012d93f060fc07
ProfSucrose <dr.blobbles@gmail.com>
2020-09-22 11:02:01 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ken-kreeper
+Night Golem for Ken Night on the Hack Club Slack
---
+++ index.js
@@ -0,0 +1,46 @@
+const { App } = require('@slack/bolt')
+const dotenv = require('dotenv')
+dotenv.config()
+
+const app = new App({
+    token: process.env.SLACK_BOT_TOKEN,
+    signingSecret: process.env.SLACK_SIGNING_SECRET
+})
+
+function nextInt(bound) {
+    return Math.floor(Math.random() * bound)
+}
+
+function formatDateForSlack(date) {
+    const timestamp = (date.getTime() / 1000).toFixed(0)
+    const linkToTime = `https://time.is/${timestamp}`
+    return `<!date^${timestamp}^{date_short_pretty} at {time}^${linkToTime}|${date}>`
+}
+
+function getRandomKenNightDate() {
+    const date = new Date()
+    date.setDate(date.getDate() + nextInt(7))
+    date.setHours(date.getDate() + nextInt(24))
+    date.setMinutes(date.getMinutes() + nextInt(60))
+    return date
+}
+
+;(async () => {
+    const port = process.env.PORT || 3000
+    await app.start(port)
+    console.log(`Started bot on port ${port}`)
+
+    app.event('message', async ({ event }) => {
+        console.log(event)
+        const text = event.text
+        const match = text.match(/k+(e|a)h*(n|l)+ (n|s)+(a|e|i|o|u)+(g|h)h*(t|d)*/gi)
+        if (match.length > 0) {
+            await app.client.chat.postMessage({
+                token: process.env.SLACK_BOT_TOKEN,
+                channel: event.channel,
+                text: `The next _${match[0]}_ is *${formatDateForSlack(getRandomKenNightDate())}*`,
+                thread_ts: event.ts
+            })
+        }
+    })
+})()
