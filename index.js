0621ebc5c1750900b95c2f1d72c9bfb9dd3543da
ProfSucrose <dr.blobbles@gmail.com>
2020-09-22 12:48:57 -0700

    Changed date formatting

--- index.js
+++ index.js
@@ -8,28 +8,42 @@ const app = new App({
 })
 
 /**
- * Generates a psuedorandom number from zero to a specified boundary.
- * @param {number} bound - Boundary of random number.
+ * Gets formatted date string from Date object
+ * @param {Date} date - Date object
+ * @returns {string}
+ */
+function getFormattedDateForSlackMessage(date) {
+    const hour = date.getHours()
+    const minute = date.getMinutes()
+
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    const dayIsToday = (new Date()).getDay() == date.getDay() 
+    const day = dayIsToday ? 'Today' : days[date.getDay()]
+    return `${!dayIsToday ? 'next' : ''} *${day} at ${hour}:${minute} ${hour >= 12 ? 'PM' : 'AM'}*`
+}
+
+/**
+ * Generates a psuedorandom number from zero to a specified boundary
+ * @param {number} bound - Boundary of random number
  * @returns {number}
  */
 function nextInt(bound) {
     return Math.floor(Math.random() * bound)
 }
 
-/**
- * Generates a formatted date string for Slack.
- * @constructor
- * @param {Date} date - Javascript Date object to be used for date string.
- * @returns {string}
- */
-function formatDateForSlack(date) {
-    const timestamp = (date.getTime() / 1000).toFixed(0)
-    const linkToTime = `https://time.is/${timestamp}`
-    return `<!date^${timestamp}^{date_short_pretty} at {time}^${linkToTime}|${date}>`
-}
+// /**
+//  * Generates a formatted date string for Slack
+//  * @param {Date} date - Javascript Date object to be used for date string
+//  * @returns {string}
+//  */
+// function formatDateForSlack(date) {
+//     const timestamp = (date.getTime() / 1000).toFixed(0)
+//     const linkToTime = `https://time.is/${timestamp}`
+//     return `<!date^${timestamp}^{date_short_pretty} at {time}^${linkToTime}|${date}>`
+// }
 
 /**
- * Generates a random Date object for Ken Night.
+ * Generates a random Date object for Ken Night
  * @returns {Date}
  */
 function getRandomKenNightDate() {
@@ -48,12 +62,13 @@ function getRandomKenNightDate() {
     app.event('message', async ({ event }) => {
         console.log(event)
         const text = event.text
-        const match = text.match(/k+(e|a)h*(n|l)+ (n|s)+(a|e|i|o|u)+(g|h)h*(t|d)*/gi)
-        if (match.length > 0) {
+        const match = text.match(/k+(e|a)h*(n|l)+ ((n|s)+(a|e|i|o|u)+(g|h)h*(t|d)*|nut+)/gi)
+        if (/next/gi.test(text)
+                && match.length > 0) {
             await app.client.chat.postMessage({
                 token: process.env.SLACK_BOT_TOKEN,
                 channel: event.channel,
-                text: `The next _${match[0]}_ is *${formatDateForSlack(getRandomKenNightDate())}*`,
+                text: `The next _${match[0]}_ is ${getFormattedDateForSlackMessage(getRandomKenNightDate())} Ken Time. See you there!`,
                 thread_ts: event.ts
             })
         }
